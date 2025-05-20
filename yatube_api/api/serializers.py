fb1f349d34b36def6af02cc61eef027fe552800e
Fedor <korotkovvl20@mail.ru>
2025-05-20 20:06:03 +0300

    init

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
+from django.contrib.auth import get_user_model
 
+from posts.models import Comment, Follow, Group, Post
 
-from posts.models import Comment, Post
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
@@ -21,3 +24,32 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('author', 'post')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault())
+    following = serializers.SlugRelatedField(
+        slug_field='username', queryset=User.objects.all())
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(), fields=('following', 'user'))
+        ]
+
+    def validate(self, data):
+        if self.context['request'].user == data['following']:
+            raise serializers.ValidationError('Нельзя подписаться на себя.')
+        return data
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,34 @@
-from django.urls import path
+from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
+from django.urls import include, path
+
+from .views import (
+    CommentViewSet,
+    PostViewSet,
+    GroupViewSet,
+    FollowViewSet
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+router = DefaultRouter()
+router.register('posts', PostViewSet, basename='posts')
+router.register('groups', GroupViewSet, basename='groups')
+router.register(r'posts/(?P<post_id>\d+)/comments',
+                CommentViewSet, basename='comments')
+router.register(r'follow', FollowViewSet)
 
 urlpatterns = [
+    path('v1/api-token-auth/', views.obtain_auth_token,
+         name='api_token'),
+    path('v1/jwt/create/', TokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+    path('v1/jwt/refresh/', TokenRefreshView.as_view(),
+         name='token_refresh'),
+    path('v1/jwt/verify/', TokenVerifyView.as_view(),
+         name='token_verify'),
+    path('v1/', include(router.urls)),
 ]
